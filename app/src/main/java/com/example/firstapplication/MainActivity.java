eb95dcb9efce145c3f6c31af813dbcec50c81de9
KaiNguyen2504 <82109977+KaiNguyen2504@users.noreply.github.com>
2021-09-14 20:01:11 +0700

    Initial commit

---
+++ app/src/main/java/com/example/firstapplication/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.firstapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editTextNum1;
+    EditText editTextNum2;
+    Button buttonOK;
+    String sKQ="";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editTextNum1 = findViewById(R.id.editTextNum1);
+        editTextNum2 = findViewById(R.id.editTextNum2);
+        buttonOK = findViewById(R.id.butOK);
+
+        buttonOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    int n1 = Integer.parseInt(editTextNum1.getText().toString());
+                    int n2 = Integer.parseInt(editTextNum2.getText().toString());
+                    sKQ= "" + (n1+n2);
+                }catch (Exception e){
+                    sKQ="Sai KQ";
+                }
+                Toast.makeText(MainActivity.this,sKQ,Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
